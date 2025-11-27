ee81b8589f188d4fbc6076cc8231d7676c7e89ad
Conflictive <callumtolland@gmail.com>
2025-11-27 03:57:12 +0000

    Implement computer choice logic

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 // Representing rock, paper and scissors
-const choices = 3
+const choices = 3;
 
 // Returns 0, 1 or 2
 function getRandomInt() {
@@ -9,6 +9,18 @@ function getRandomInt() {
 }
 
 function getComputerChoice() {
-
+    switch(getRandomInt()) {
+        case 0:
+            console.log('Rock')
+            break;
+        case 1:
+            console.log('Paper')
+            break;
+        case 2:
+            console.log('Scissors')
+            break;
+    }
 }
+
+getComputerChoice()
 
