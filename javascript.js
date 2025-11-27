66d1c73a999d053cdee92209623e65484ed1285c
Conflictive <callumtolland@gmail.com>
2025-11-27 03:44:24 +0000

    Implement function for random number selection
    Method for the computer selecting rock, paper or scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("Test")
+// Representing rock, paper and scissors
+const choices = 3
+
+// Returns 0, 1 or 2
+function getRandomInt() {
+
+  return Math.floor(Math.random() * choices);
+  
+}
+
+function getComputerChoice() {
+
+}
+
